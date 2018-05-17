ffbfc92bea032a8a29939bfa222f58cc7fea357d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-17 17:19:27 +1000

    Refactor authenticate

--- app.js
+++ app.js
@@ -9,21 +9,34 @@ const org = nforce.createConnection({
   mode: 'multi'
 })
 
-// Authenticate using multi user mode
-let oauth;
-org.authenticate({
-  username: process.env.SALESFORCE_USERNAME,
-  password: process.env.SALESFORCE_PASSWORD,
-  securityToken: process.env.SALESFORCE_SECURITY_TOKEN
-}, (err, resp) => {
+const authenticate = (username, password, securityToken) => (
+  new Promise( (resolve, reject) => {
+    // Authenticate using multi user mode
+    org.authenticate({
+      username: username,
+      password: password,
+      securityToken: securityToken
+    }, (error, response) => {
+      if(!error) {
+        resolve(response)
+      }
+      else {
+        reject(error)
+      }
+    })
+  })
+)
 
-  // Check for authentication errors
-  if(!err) {
-    oauth = resp
-    console.log('oauth: '+oauth)
+const run = async () => {
+  try {
+    let oauth = await authenticate(
+      process.env.SALESFORCE_USERNAME,
+      process.env.SALESFORCE_PASSWORD,
+      process.env.SALESFORCE_SECURITY_TOKEN)
 
     // Perform a query
     let q = 'SELECT Id, Name FROM Account LIMIT 10'
+
     org.query({ query: q, oauth: oauth }, (err, resp) => {
       
       if(!err && resp.records) {
@@ -32,8 +45,43 @@ org.authenticate({
       }
     })
   }
-  else {
-    // Output the error
-    console.error('err: '+err)
+  catch(error) {
+    console.error(error)
   }
-})
+  
+}
+
+run()
+
+// // Authenticate using multi user mode
+// let oauth = await authenticate(
+//               process.env.SALESFORCE_USERNAME,
+//               process.env.SALESFORCE_PASSWORD,
+//               process.env.SALESFORCE_SECURITY_TOKEN)
+
+// org.authenticate({
+//   username: process.env.SALESFORCE_USERNAME,
+//   password: process.env.SALESFORCE_PASSWORD,
+//   securityToken: process.env.SALESFORCE_SECURITY_TOKEN
+// }, (err, resp) => {
+
+//   // Check for authentication errors
+//   if(!err) {
+//     oauth = resp
+//     console.log('oauth: '+oauth)
+
+//     // Perform a query
+//     let q = 'SELECT Id, Name FROM Account LIMIT 10'
+//     org.query({ query: q, oauth: oauth }, (err, resp) => {
+      
+//       if(!err && resp.records) {
+//         // Output the records
+//         console.log(resp.records)
+//       }
+//     })
+//   }
+//   else {
+//     // Output the error
+//     console.error('err: '+err)
+//   }
+// })
