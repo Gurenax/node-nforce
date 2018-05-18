853e49d1de501c8f316f651574b30416a0365c6d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-18 10:59:20 +1000

    Add createRecord function

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const authenticate = (username, password, securityToken) =>
 /**
  * Perform a SOQL Query
  * @param {*} queryString - SOQL query
- * @param {*} oauth - OAuth string received from successful authentication
+ * @param {*} oauth       - OAuth string received from successful authentication
  */
 const query = (queryString, oauth) =>
   new Promise((resolve, reject) => {
@@ -60,12 +60,46 @@ const query = (queryString, oauth) =>
     )
   })
 
+const sampleData = {
+  Name: 'Spiffy Cleaners',
+  Phone: '800-555-2345',
+  SLA__c: 'Gold'
+}
+
+/**
+ * Create a record
+ * @param {*} sobjectName - Name of the SObject (e.g. Account)
+ * @param {*} recordData  - Key value pair of Field Names and Field Values
+ * @param {*} oauth       - OAuth string received from successful authentication
+ */
+const createRecord = (sobjectName, recordData, oauth) => {
+  const sobj = nforce.createSObject(sobjectName)
+  Object.entries(recordData).map(([key, value]) => {
+    sobj.set(key, value)
+  })
+  return new Promise((resolve, reject) => {
+    org.insert(
+      {
+        sobject: sobj,
+        oauth: oauth
+      },
+      (error, response) => {
+        if (!error) {
+          resolve(response)
+        } else {
+          reject(error)
+        }
+      }
+    )
+  })
+}
+
 /**
  * Run the app
  */
 const run = async () => {
   try {
-    let oauth = await authenticate(
+    const oauth = await authenticate(
       process.env.SALESFORCE_USERNAME,
       process.env.SALESFORCE_PASSWORD,
       process.env.SALESFORCE_SECURITY_TOKEN
@@ -74,6 +108,17 @@ const run = async () => {
     // Perform a query
     let records = await query('SELECT Id, Name FROM Account LIMIT 10', oauth)
     console.log(records)
+
+    // Create a record
+    let account = await createRecord('Account', sampleData, oauth)
+    console.log(account)
+
+    // Perform account query
+    let accountRecord = await query(
+      `SELECT Id, Name, SLA__c FROM Account WHERE Id = '${account.id}'`,
+      oauth
+    )
+    console.log(accountRecord)
   } catch (error) {
     console.error(error)
   }
